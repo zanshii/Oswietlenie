6936182c6e1e11e9aa36eafee3648b4a337291c6
loyban <adaarc@gmail.com>
2014-01-25 17:54:48 +0100

    zmiana sposobu rysowania sfery
    teraz wystarczy wpiac wzorki na phonga i tyle (chyba)

--- OpenTKProject/OpenTKProject/MainWindow.cs
+++ OpenTKProject/OpenTKProject/MainWindow.cs
@@ -3,18 +3,18 @@ using System.Collections.Generic;
 using System.Text;
 using System.Drawing;
 using OpenTK;
-using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
 using OpenTK.Input;
-
+#pragma warning disable 618
 namespace ConsoleApplication1
 {
     class MainWindow : GameWindow
     {
-        float[] ambientLight = new float[4] { 0.2f, 0.2f, 0.2f, 1.0f };
-        float[] diffuseLight = new float[4] { 0.7f, 0.7f, 0.7f, 1.0f };
-        float[] noLight = new float[4] { 0.0f, 0.0f, 0.0f, 1.0f };
-        float[] lightPos = new float[4] { 200.0f, 300.0f, 100.0f, 1.0f };
-        float[] cameraPos = new float[4] { 100.0f, 150.0f, 200.0f, 1.0f };
+        Vector4 ambientLight = new Vector4(0.2f, 0.2f, 0.2f, 1f);
+        Vector4 diffuseLight = new Vector4(0.7f, 0.7f, 0.7f, 1f);
+        Vector4 noLight = new Vector4(0.0f, 0.0f, 0.0f, 1f);
+        Vector3 lightPos = new Vector3(200.0f, 300.0f, 100.0f);
+        Vector3 cameraPos = new Vector3(100.0f, 150.0f, 200.0f);
 
         float factor = 5.0f;
 
@@ -23,14 +23,12 @@ namespace ConsoleApplication1
         int windowWidth = 512;                // window size
         int windowHeight = 512;
 
-        IntPtr esfera;
 
         float rot = 0;
 
         public MainWindow()
             : base(800, 600)
         {
-            esfera = Glu.NewQuadric();
             Keyboard.KeyDown += Keyboard_KeyDown;
         }
 
@@ -75,19 +73,74 @@ namespace ConsoleApplication1
             width /= 2.0f;
             height /= 2.0f;
             length /= 2.0f;
-            
-            // Draw green sphere
-            GL.Color3(0.0f, 1.0f, 0.0f);
-            GL.PushMatrix();
-            GL.Translate(-60.0f, 0.0f, 0.0f);
-            Glu.Sphere(esfera, 25.0f, 50, 50);
-            GL.PopMatrix();
+            Vector3 Center = new Vector3(100, 0, 0);
+            float Radius = 32;
+            uint Precision = 64;
 
-            
+
+            if (Radius < 0f)
+                Radius = -Radius;
+            if (Radius == 0f)
+                throw new DivideByZeroException("DrawSphere: Radius cannot be 0f.");
+            if (Precision == 0)
+                throw new DivideByZeroException("DrawSphere: Precision of 8 or greater is required.");
+
+            const float HalfPI = (float)(Math.PI * 0.5);
+            float OneThroughPrecision = 1.0f / Precision;
+            float TwoPIThroughPrecision = (float)(Math.PI * 2.0 * OneThroughPrecision);
+
+            float theta1, theta2, theta3;
+
+            Vector3 Normal, Position;
+
+            for (uint j = 0; j < Precision / 2; j++)
+            {
+                theta1 = (j * TwoPIThroughPrecision) - HalfPI;
+                theta2 = ((j + 1) * TwoPIThroughPrecision) - HalfPI;
+
+                GL.Begin(BeginMode.TriangleStrip);
+                for (uint i = 0; i <= Precision; i++)
+                {
+                    theta3 = i * TwoPIThroughPrecision;
+
+                    Normal.X = (float)(Math.Cos(theta2) * Math.Cos(theta3));
+                    Normal.Y = (float)Math.Sin(theta2);
+                    Normal.Z = (float)(Math.Cos(theta2) * Math.Sin(theta3));
+                    Position.X = Center.X + Radius * Normal.X;
+                    Position.Y = Center.Y + Radius * Normal.Y;
+                    Position.Z = Center.Z + Radius * Normal.Z;
+                    
+                    //tutaj zmienić kolor na policzony z phonga
+                    //GL.Color4(a,b,c,d)
+
+                    GL.Normal3(Normal);
+                    GL.TexCoord2(i * OneThroughPrecision, 2.0f * (j + 1) * OneThroughPrecision);
+                    GL.Vertex3(Position);
+
+
+                    Normal.X = (float)(Math.Cos(theta1) * Math.Cos(theta3));
+                    Normal.Y = (float)Math.Sin(theta1);
+                    Normal.Z = (float)(Math.Cos(theta1) * Math.Sin(theta3));
+                    Position.X = Center.X + Radius * Normal.X;
+                    Position.Y = Center.Y + Radius * Normal.Y;
+                    Position.Z = Center.Z + Radius * Normal.Z;
+
+                    //tutaj zmienić kolor na policzony z phonga
+                    //GL.Color4(a,b,c,d)
+
+                    GL.Normal3(Normal);
+                    GL.TexCoord2(i * OneThroughPrecision, 2.0f * j * OneThroughPrecision);
+                    GL.Vertex3(Position);
+
+
+                }
+                GL.End();
+            }
+
         }
         #endregion DrawModels()
 
-       #region OnLoad
+        #region OnLoad
 
         /// <summary>
         /// Setup OpenGL and load resources here.
@@ -115,8 +168,6 @@ namespace ConsoleApplication1
         /// <remarks>There is no need to call the base implementation.</remarks>
         protected override void OnResize(EventArgs e)
         {
-            int i;
-
             windowWidth = Width;
             windowHeight = Height;
         }
@@ -150,17 +201,14 @@ namespace ConsoleApplication1
             // Track camera angle
             GL.MatrixMode(MatrixMode.Projection);
             GL.LoadIdentity();
-            Glu.Perspective(45.0f, aspect, 1.0f, 1000.0f);
+            OpenTK.Graphics.Glu.Perspective(45.0f, aspect, 1.0f, 1000.0f);
             GL.MatrixMode(MatrixMode.Modelview);
             GL.LoadIdentity();
-            Glu.LookAt(cameraPos[0], cameraPos[1], cameraPos[2],
+            OpenTK.Graphics.Glu.LookAt(cameraPos[0], cameraPos[1], cameraPos[2],
                       0.0f, 0.0f, 0.0f, 0.0f, 1.0f, 0.0f);
             GL.Viewport(0, 0, windowWidth, windowHeight);
 
             GL.Rotate(rot, 0, 1, 0);
-
-            // Track light position
-            GL.Light(LightName.Light0, LightParameter.Position, lightPos);
 
             // Clear the window with current clearing color
             GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
@@ -182,8 +230,6 @@ namespace ConsoleApplication1
         protected override void OnUnload(EventArgs e)
         {
             base.OnUnload(e);
-
-            Glu.DeleteQuadric(esfera);
         }
-}
+    }
 }
